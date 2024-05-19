a070dc28d358386845a618fa576f212c4ceaada3
GarthTeixeira <garthur1998@gmail.com>
2024-05-19 12:47:47 -0300

    adjust to insert archive

--- src/app/components/form-competencias-cognitivas/form-competencias-cognitivas.component.ts
+++ src/app/components/form-competencias-cognitivas/form-competencias-cognitivas.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-form-competencias-cognitivas',
   standalone: true,
-  imports: [],
+  imports: [MatIcon, MatIconModule, MatButtonModule],
   templateUrl: './form-competencias-cognitivas.component.html',
   styleUrl: './form-competencias-cognitivas.component.scss'
 })
 export class FormCompetenciasCognitivasComponent {
 
+  fileName = '';
+
+  fileChanged(event: any){
+    const files = event.target.files;
+    if (files){
+      this.fileName = files[0].name;
+      console.log(files);
+    } else {
+      
+    }
+  }
 }
