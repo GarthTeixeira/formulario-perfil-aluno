b4e2a0f32a1d45be70ac9306222b22b9e49dfa7c
GarthTeixeira <garthur1998@gmail.com>
2025-01-06 20:30:14 -0300

    Add check if disciplina was responded

--- src/app/components/selector-component/selector-component.component.ts
+++ src/app/components/selector-component/selector-component.component.ts
@@ -4,15 +4,21 @@ import { RouterModule } from '@angular/router';
 import { DecodeUTF8Pipe } from '../../pipes/decode-utf8.pipe';
 import { Disciplina } from '../../types/serviceTypes';
 import { AreasSelectItem, DisciplinaSelectItem } from '../../types/componentsTypes';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-selector-component',
   standalone: true,
-  imports: [RouterModule, NgClass, DecodeUTF8Pipe],
+  imports: [RouterModule, NgClass, DecodeUTF8Pipe, MatIconModule],
   templateUrl: './selector-component.component.html',
   styleUrl: './selector-component.component.scss'
 })
 export class SelectorComponent {
-@Input() public items:Array<DisciplinaSelectItem | AreasSelectItem>  = [];
+@Input() public items:Array<DisciplinaSelectItem | AreasSelectItem >  = [];
 @Input() public destinyRoute:string = '';
+
+isAnswered(item: DisciplinaSelectItem | AreasSelectItem): boolean {
+  return 'answered' in item && item.answered;
+}
+
 }
