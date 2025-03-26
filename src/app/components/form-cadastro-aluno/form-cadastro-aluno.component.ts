9fb4756c1f452c2c8cca595b733b5b34889103b1
GarthTeixeira <garthur1998@gmail.com>
2025-03-26 11:26:02 -0300

    Aplicando componentização no campo do formulário carregável

--- src/app/components/form-cadastro-aluno/form-cadastro-aluno.component.ts
+++ src/app/components/form-cadastro-aluno/form-cadastro-aluno.component.ts
@@ -4,8 +4,8 @@ import { MatButtonModule } from '@angular/material/button';
 import {  MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import {  MatSelectModule } from '@angular/material/select';
-import { FormBuilder,  FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {  MatSelectChange, MatSelectModule } from '@angular/material/select';
+import { FormBuilder,  FormControl,  FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { DataSharedService } from '../../shared/data-shared.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -17,7 +17,7 @@ import { FormProfessoresService } from '../../services/form-professores.service'
 import { EscolasService } from '../../services/escolas.service';
 import { LocalStorageService } from '../../shared/services/local-storage-service.service';
 import { UserDataLocalStorage } from '../../types/localStorageTypes';
-import { DecodeUTF8Pipe } from '../../pipes/decode-utf8.pipe';
+import { LoadingFormFieldComponent } from '../loading-form-field/loading-form-field.component';
 @Component({
   selector: 'app-form-cadastro-aluno',
   standalone: true,
@@ -30,8 +30,7 @@ import { DecodeUTF8Pipe } from '../../pipes/decode-utf8.pipe';
     MatButtonModule,
     ReactiveFormsModule,
     MatProgressSpinnerModule,
-    DecodeUTF8Pipe,
-    MatProgressSpinnerModule
+    LoadingFormFieldComponent
   ],
   templateUrl: './form-cadastro-aluno.component.html',
   styleUrl: './form-cadastro-aluno.component.scss'
@@ -50,7 +49,7 @@ export class FormCadastroAlunoComponent {
 
   public turmasOptions: any[] = []
 
-  public loadingData:Observable<any> | null = null
+  public loadingSchoolData:Observable<any> | null = null
 
   public errorLoadingSchools: boolean = false;
 
@@ -66,13 +65,11 @@ export class FormCadastroAlunoComponent {
     private _professoresService: FormProfessoresService,
     private _escolasService: EscolasService
   ) {
-    this.loadingData = this._escolasService.getEscolasOptions().pipe(
-      catchError((error) => {
-        this.errorLoadingSchools = true;
-        console.error('Erro ao carregar escolas:', error);
-        return of([]); 
-      })
-    );
+    this.loadingSchoolData = this._escolasService.getEscolasOptions()
+  }
+
+  get escolaControl(): FormControl {
+    return this.applyForm.get('escola') as FormControl;
   }
 
   ngOnInit() {
@@ -88,13 +85,11 @@ export class FormCadastroAlunoComponent {
       turma: [{value:'', disabled:true}, Validators.required],
     })
 
-    this.loadingData?.subscribe((response) => {
-        this.escolasOptions = response
-      });
   }
 
-  onChangeEscola(value: string){
-    this.selectedSchool = this.escolasOptions.find(escola=>escola.id == value)
+  onChangeEscola(event: MatSelectChange){
+
+    this.selectedSchool = event.value
     
     this.serieOptions = [ ... new Set(
       this.selectedSchool?.turmas.map((turma: any) => turma?.serie || '') || []
---
+++ src/app/components/loading-form-field/loading-form-field.component.ts
@@ -0,0 +1,66 @@
+import { CommonModule } from '@angular/common';
+import { EventEmitter, Component, Input, Output, ViewEncapsulation } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { Observable, catchError, of } from 'rxjs';
+import { DecodeUTF8Pipe } from '../../pipes/decode-utf8.pipe';
+
+@Component({
+  selector: 'app-loading-form-field',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    DecodeUTF8Pipe
+  ],
+  templateUrl: './loading-form-field.component.html',
+  styleUrl: './loading-form-field.component.scss',
+  encapsulation: ViewEncapsulation.None
+
+})
+export class LoadingFormFieldComponent {
+
+  @Input() public control!: FormControl
+
+  @Input() public fetchData:Observable<any> | null = null
+
+  @Input() public itemsOptions: any[] = []
+
+  @Output() selectionChange = new EventEmitter<any>();
+
+  public errorLoadingItems: boolean = false;
+
+  public loadingData:Observable<any> | null | undefined = null
+
+  @Input() icon = 'arrow_drop_down';
+
+  @Input() label = 'Selecione';
+  @Input() optionDisplayField = 'name';
+
+
+  ngOnInit(){
+    this.loadingData = this.fetchData?.pipe(
+      catchError((error) => {
+        this.errorLoadingItems = true;
+        console.error(`Erro ao carregar entidade '${this.label.toLocaleLowerCase()}':`, error);
+        return of([]); 
+      })
+    );
+
+    this.loadingData?.subscribe((response) => {
+      this.itemsOptions = response
+    });
+  }
+
+  onSelectionChange(event: any){
+    this.selectionChange.emit(event)
+  }
+
+}
