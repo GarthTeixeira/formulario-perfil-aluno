dcd8e345bcceade7f772dd92ce7154d4fb24f340
GarthTeixeira <garthur1998@gmail.com>
2025-04-02 10:55:41 -0300

    formatando

--- src/app/components/loading-form-field/loading-form-field.component.ts
+++ src/app/components/loading-form-field/loading-form-field.component.ts
@@ -1,5 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { EventEmitter, Component, Input, Output, ViewEncapsulation } from '@angular/core';
+import {
+  EventEmitter,
+  Component,
+  Input,
+  Output,
+  ViewEncapsulation,
+} from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -18,49 +24,48 @@ import { DecodeUTF8Pipe } from '../../pipes/decode-utf8.pipe';
     MatSelectModule,
     MatIconModule,
     MatProgressSpinnerModule,
-    DecodeUTF8Pipe
+    DecodeUTF8Pipe,
   ],
   templateUrl: './loading-form-field.component.html',
   styleUrl: './loading-form-field.component.scss',
-  encapsulation: ViewEncapsulation.None
-
+  encapsulation: ViewEncapsulation.None,
 })
 export class LoadingFormFieldComponent {
+  @Input() public control!: FormControl;
 
-  @Input() public control!: FormControl
+  @Input() public fetchData: Observable<any> | null = null;
 
-  @Input() public fetchData:Observable<any> | null = null
-
-  @Input() public itemsOptions: any[] = []
+  @Input() public itemsOptions: any[] = [];
 
   @Output() selectionChange = new EventEmitter<any>();
 
   public errorLoadingItems: boolean = false;
 
-  public loadingData:Observable<any> | null | undefined = null
+  public loadingData: Observable<any> | null | undefined = null;
 
   @Input() icon = 'arrow_drop_down';
 
   @Input() label = 'Selecione';
   @Input() optionDisplayField = 'name';
 
-
-  ngOnInit(){
+  ngOnInit() {
     this.loadingData = this.fetchData?.pipe(
       catchError((error) => {
         this.errorLoadingItems = true;
-        console.error(`Erro ao carregar entidade '${this.label.toLocaleLowerCase()}':`, error);
-        return of([]); 
+        console.error(
+          `Erro ao carregar entidade '${this.label.toLocaleLowerCase()}':`,
+          error
+        );
+        return of([]);
       })
     );
 
     this.loadingData?.subscribe((response) => {
-      this.itemsOptions = response
+      this.itemsOptions = response;
     });
   }
 
-  onSelectionChange(event: any){
-    this.selectionChange.emit(event)
+  onSelectionChange(event: any) {
+    this.selectionChange.emit(event);
   }
-
 }
